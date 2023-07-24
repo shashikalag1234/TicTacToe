8ff4dc2833bc58bddd8cd63ae7af62f1bea506ac
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-24 05:40:43 +0530

    Refractor: Extract First player name from string literals.

--- src/test/java/com/ttt/tdd/TicTacToeTest.java
+++ src/test/java/com/ttt/tdd/TicTacToeTest.java
@@ -17,6 +17,8 @@ public class TicTacToeTest {
 
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
+	public static final Character FIRST_PLAYER = 'X';
+
 
 	@Test
 	public void initializeNewTicTacToeGame() {
@@ -37,7 +39,7 @@ public class TicTacToeTest {
 
 	@Test
 	public void playWithFIrstPlayer() {
-		assertEquals('X', ticTacToe.nextPlayer());
+		assertEquals(FIRST_PLAYER, ticTacToe.nextPlayer());
 	}
 
 }
